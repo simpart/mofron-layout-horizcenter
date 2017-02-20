4b1f512b77d79fcdb7cca6fda93b513d4474699e
simpart <taria.ki4m@gmail.com>
2017-02-20 20:35:59 +0900

    add comment

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 /**
- * @file hrzcenter.js
+ * @file mofron-layout-hrzcenter/index.js
+ * @author simpart
+ */
+
+/**
+ * @class HrzCenter
+ * @brief horizon center layout
  */
 mofron.layout.HrzCenter = class extends mofron.Layout {
+    
     constructor (rt) {
         try {
             super();
-            this.rate = (rt === undefined) ? 80 : rt;
+            this.m_rate = null;
+            this.rate((rt === undefined) ? 80 : rt);
         } catch (e) {
             console.error(e.stack);
             throw e;
@@ -14,16 +22,32 @@ mofron.layout.HrzCenter = class extends mofron.Layout {
     
     layoutConts (idx, tgt) {
         try {
-            tgt.style('width', '100%');
-            if ('fixed' === tgt.style('position')) {
-                tgt.style('position');
-            }
-            tgt.style('width'   , this.rate + '%');
+            tgt.style('width'   , this.rate() + '%');
             tgt.style('position', 'relative');
-            tgt.style('left'    , (100 - this.rate)/2 + '%');
+            tgt.style('left'    , (100 - this.rate())/2 + '%');
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    rate (rt) {
+        try {
+            if (undefined === rt) {
+                /* getter */
+                return this.m_rate;
+            }
+            /* setter */
+            if ( ('number' !== typeof rt) ||
+                 (       0  >  rt)        ||
+                 (      99  <  rt) ) {
+                throw new Error('invalid parameter');
+            }
+            this.m_rate = rt;
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
 }
+module.exports = mofron.layout.HrzCenter;
