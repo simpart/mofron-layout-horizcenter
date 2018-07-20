5913b480d6b16eb134a61ce48e5500061df23943
root <root>
2018-07-20 19:16:20 +0900

    rename
    layoutConts -> contents

--- index.js
+++ index.js
@@ -13,91 +13,65 @@ mofron.layout.HrzCenter = class extends mofron.Layout {
         try {
             super();
             this.name('HrzCenter');
+            this.prmOpt(po);
             
-            this.prmCheck(
-                (p1) => {
+            this.getParam().check(
+                /* rate */
+                (rt) => {
                     try {
-                        if ('number' !== typeof p1) {
+                        if ('number' !== typeof rt) {
+                            throw new Error('invalid parameter');
+                        }
+                        if ((0 > rt) || (100 < rt)) {
                             throw new Error('invalid parameter');
                         }
                     } catch (e) {
                         console.error(e.stack);
                         throw e;
                     }
-                }
+                },
+                /* type */
+                (tp) => {
+                    try {
+                        if (undefined === tp) {
+                            return 'relative';
+                        }
+                        if ('string' !== typeof tp) {
+                            throw new Error('invalid parameter');
+                        }
+                        if (('relative' !== tp) && ('margin' !== tp) && ('padding' !== tp)) {
+                            throw new Error('invalid parameter');
+                        }
+                    } catch (e) {
+                        console.error(e.stack);
+                        throw e;
+                    }
+                },
             );
             
-            this.prmOpt(po);
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
     
-    layoutConts (idx, tgt) {
+    contents (idx, tgt) {
         try {
-            let prm = this.param()[0];
-            if (null !== prm) {
-                let type = this.type();
-                if (null === type) {
-                    tgt.adom().style({
-                        position : 'relative',
-                        left     : (100 - prm)/2 + '%'
-                    });
-                } else {
-                    var set_style = {};
-                    set_style[type+'-left'] =  (100 - prm)/2 + '%';
-                    tgt.adom().style(set_style);
-                }
-                if ('function' === typeof tgt.width) {
-                    tgt.width(prm + '%');
-                } else {
-                    tgt.adom().style({
-                        width    : prm + '%',
-                    });
-                }
-            } else if (null !== this.width()) {
-                tgt.style({
-                    position       : 'relative',
-                    'margin-left'  : 'auto',
-                    'margin-right' : 'auto'
+            let rate = this.value()[0];
+            let type = this.value()[1];
+            
+            if ('relative' === type) {
+                tgt.adom().style({
+                    position : type,
+                    left     : (100 -rate)/2 + '%'
                 });
+            } else {
+                let set_style = {};
+                set_style[type+'-left'] =  (100 - rate)/2 + '%';
+                tgt.adom().style(set_style);
             }
-        } catch (e) {
-            console.error(e.stack);
-            throw e;
-        }
-    }
-    
-    width (wid) {
-        try {
-            if (undefined === wid) {
-                /* getter */
-                return (undefined === this.m_width) ? null : this.m_width;
-            }
-            /* setter */
-            if (('number' !== typeof wid) && ('string' !== typeof wid)) {
-                throw new Error('invalid parameter');
-            }
-            this.m_width = wid;
-        } catch (e) {
-            console.error(e.stack);
-            throw e;
-        }
-    }
-    
-    type (tp) {
-        try {
-            if (undefined === tp) {
-                /* getter */
-                return (undefined === this.m_type) ? null : this.m_type;
-            }
-            /* setter */
-            if ( ('string' !== typeof tp) ||
-                 !(('padding' === tp) || ('margin' === tp)) ) {
-                throw new Error('invalid parameter');
-            }
-            this.m_type = tp;
+            
+            tgt.width(rate + '%');
         } catch (e) {
             console.error(e.stack);
             throw e;
