f20203b62cc1b94aa4c8d42d381db5255827c98c
simpart <simpart>
2018-10-20 11:58:09 +0000

    add comment

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 /**
  * @file mofron-layout-hrzcenter/index.js
+ * @brief horizon center layout
  * @author simpart
  */
 const mf = require('mofron');
-/**
- * @class HrzCenter
- * @brief horizon center layout
- */
+
 mf.layout.HrzCenter = class extends mf.Layout {
-    
+    /**
+     * initialize horizon center layout
+     * 
+     * @param p1 (object) layout option
+     * @param p1 (number) center rate
+     * @param p2 (string) center type
+     */
     constructor (po, p2) {
         try {
             super();
             this.name('HrzCenter');
-            this.prmMap('rate', 'type');
+            this.prmMap(['rate', 'type']);
             this.prmOpt(po, p2);
         } catch (e) {
             console.error(e.stack);
@@ -21,55 +25,61 @@ mf.layout.HrzCenter = class extends mf.Layout {
         }
     }
     
+    /**
+     * layout contents
+     *
+     * @note private method
+     */
     contents (idx, tgt) {
         try {
             if ('relative' === this.type()) {
                 tgt.adom().style({
                     position : this.type(),
-                    left     : (100 - this.rate().value())/2 + '%'
+                    left     : (100 - this.rate())/2 + '%'
                 });
             } else {
                 let set_style = {};
-                set_style[this.type() + '-left'] =  (100 - this.rate().value())/2 + '%';
+                set_style[this.type() + '-left'] =  (100 - this.rate())/2 + '%';
                 tgt.adom().style(set_style);
             }
-            tgt.width(this.rate());
+            tgt.width(this.rate() + '%');
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
     
+    /**
+     * setter/getter center rate
+     *
+     * @param p1 (number) center rate 
+     * @param p1 (undefined) call as getter
+     * @return (number) center rate
+     */
     rate (prm) {
         try {
-            if (undefined === prm) {
-                /* getter */
-                return (undefined === this.m_rate) ? '80%' : this.m_rate;
+            let ret = this.member('rate', 'number', prm, 80);
+            if ( (undefined !== prm) &&
+                 ( (0 > prm) || (100 < prm))) {
+                throw new Error('invalid parameter');
             }
-            /* setter */
-            this.m_rate = mf.func.getSizeObj(
-                ('number' === typeof prm) ? prm + '%' : prm
-            );
+            return ret;
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
     
+    /**
+     * setter/getter center type
+     *
+     * @param p1 (string) center type
+     * @param p1 (undefined) call as getter
+     * @return (string) center type
+     */
     type (prm) {
         try {
-            if (undefined === prm) {
-                /* getter */
-                return (undefined === this.m_type) ? 'relative' : this.m_type;
-            }
-            /* setter */
-            if ('string' !== typeof prm) {
-                throw new Error('invalid parameter');
-            }
-            if (('relative' !== prm) && ('margin' !== prm) && ('padding' !== prm)) {
-                throw new Error('invalid parameter');
-            }
-            this.m_type = prm;
+            return this.member('type', ['relative', 'margin', 'padding'], prm, 'relative');
         } catch (e) {
             console.error(e.stack);
             throw e;
